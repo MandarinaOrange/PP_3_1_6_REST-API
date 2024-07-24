548585b61bb8f8f3a621f570e16528503fb647f4
Mandarin <msdolphin@yandex.ru>
2024-07-24 21:42:03 +0300

    ???

--- PP_3_1_6_REST-API/src/main/java/com/mandarin/PP_3_1_6_REST_API/controller/RestController.java
+++ PP_3_1_6_REST-API/src/main/java/com/mandarin/PP_3_1_6_REST_API/controller/RestController.java
@@ -3,14 +3,8 @@ package com.mandarin.PP_3_1_6_REST_API.controller;
 import com.mandarin.PP_3_1_6_REST_API.model.User;
 import com.mandarin.PP_3_1_6_REST_API.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
 
 @org.springframework.web.bind.annotation.RestController
