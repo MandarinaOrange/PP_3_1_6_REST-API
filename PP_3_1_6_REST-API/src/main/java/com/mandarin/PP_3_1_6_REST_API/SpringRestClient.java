b1bc5e9f7e41b79bc611d7a4f6c10a1b4fffa5a0
Mandarin <msdolphin@yandex.ru>
2024-07-25 17:39:42 +0300

    обоже блет заработало создание

--- PP_3_1_6_REST-API/src/main/java/com/mandarin/PP_3_1_6_REST_API/SpringRestClient.java
+++ PP_3_1_6_REST-API/src/main/java/com/mandarin/PP_3_1_6_REST_API/SpringRestClient.java
@@ -9,32 +9,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SpringRestClient {
-    private static final String GET_EMPLOYEES_ENDPOINT_URL = "http://localhost:8080/api/users";
-    private static final String GET_EMPLOYEE_ENDPOINT_URL = "http://localhost:8080/api/users/{id}";
-    private static final String CREATE_EMPLOYEE_ENDPOINT_URL = "http://localhost:8080/api/users";
-    private static final String UPDATE_EMPLOYEE_ENDPOINT_URL = "http://localhost:8080/api/users";
-    private static final String DELETE_EMPLOYEE_ENDPOINT_URL = "http://localhost:8080/api/users/{id}";
+    private static final String GET_EMPLOYEES_ENDPOINT_URL = "http://94.198.50.185:7081/api/users";
+    private static final String GET_EMPLOYEE_ENDPOINT_URL = "http://94.198.50.185:7081/api/users/{id}";
+    private static final String CREATE_EMPLOYEE_ENDPOINT_URL = "http://94.198.50.185:7081/api/users";
+    private static final String UPDATE_EMPLOYEE_ENDPOINT_URL = "http://94.198.50.185:7081/api/users";
+    private static final String DELETE_EMPLOYEE_ENDPOINT_URL = "http://94.198.50.185:7081/api/users/{id}";
     private static RestTemplate restTemplate = new RestTemplate();
+    private static String cookie;
 
 
 
     public static void main(String[] args) {
         SpringRestClient springRestClient = new SpringRestClient();
 
+        springRestClient.getUsers();
         // Step1: first create a new employee
         springRestClient.createUser();
 
         // Step 2: get new created employee from step1
-        springRestClient.getUserById();
+        //springRestClient.getUserById();
 
         // Step3: get all employees
-        springRestClient.getUsers();
+        //springRestClient.getUsers();
 
         // Step4: Update employee with id = 3
-        springRestClient.updateUser();
+        //springRestClient.updateUser();
 
         // Step5: Delete employee with id = 3
-        springRestClient.deleteUser();
+        //springRestClient.deleteUser();
     }
     private void getUsers() {
 
@@ -44,14 +46,15 @@ public class SpringRestClient {
 
         ResponseEntity<String> result = restTemplate.exchange(GET_EMPLOYEES_ENDPOINT_URL, HttpMethod.GET, entity,
                 String.class);
+        cookie = result.getHeaders().get("Set-Cookie").toString();
 
-        System.out.println(result);
+        System.out.println(cookie);
     }
 
     private void getUserById() {
 
         Map<String, String> params = new HashMap<String, String>();
-        params.put("id", "3");
+        params.put("id", "2");
 
         RestTemplate restTemplate = new RestTemplate();
         User result = restTemplate.getForObject(GET_EMPLOYEE_ENDPOINT_URL, User.class, params);
@@ -61,26 +64,65 @@ public class SpringRestClient {
 
     private void createUser() {
 
-        User newUser = new User(3L,"James", "Brown", (byte) 55);
+        HttpHeaders headers = new HttpHeaders();
+        //headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("Cookie", cookie);
 
-        RestTemplate restTemplate = new RestTemplate();
-        User result = restTemplate.postForObject(CREATE_EMPLOYEE_ENDPOINT_URL, newUser, User.class);
 
-        System.out.println(result);
+        User newUser = new User();
+        //newUser.setId(3L);
+        newUser.setName("James");
+        newUser.setLastName("Brown");
+        newUser.setAge((byte) 18);
+
+
+
+        HttpEntity<User> requestEntity = new HttpEntity<>(newUser, headers);
+        ResponseEntity<String> responseEntity = restTemplate.exchange(CREATE_EMPLOYEE_ENDPOINT_URL,
+                HttpMethod.POST, requestEntity, String.class);
+        System.out.println(responseEntity.getBody());
+
+
+        //RestTemplate restTemplate = new RestTemplate();
+        //User result = restTemplate.postForObject(CREATE_EMPLOYEE_ENDPOINT_URL, newUser, User.class);
+
+        //System.out.println(result);
     }
 
     private void updateUser() {
-        Map<String, String> params = new HashMap<String, String>();
-        //params.put("id", "3");
-        User updatedUser = new User(3L,"Thomas", "Shelby", (byte) 55);
+        //Map<String, String> params = new HashMap<String, String>();
+        //params.put("id", "2");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        //HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        headers.add("Cookie", cookie);
+
+
+        User updatedUser = new User((long) 2,"Thomas", "Shelby", (byte) 55);
         RestTemplate restTemplate = new RestTemplate();
+
         restTemplate.put(UPDATE_EMPLOYEE_ENDPOINT_URL, updatedUser);
+
+
+        System.out.println(cookie);
+
+
     }
 
     private void deleteUser() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        headers.add("Cookie", cookie);
+        //headers.add("X-HTTP-Method-Override", "DELETE");
+
+
         Map<String, String> params = new HashMap<String, String>();
-        params.put("id", "3");
+        params.put("id", "2");
         RestTemplate restTemplate = new RestTemplate();
-        restTemplate.delete(DELETE_EMPLOYEE_ENDPOINT_URL, params);
+        //restTemplate.delete(DELETE_EMPLOYEE_ENDPOINT_URL, params);
+        ResponseEntity<String> result = restTemplate.exchange(DELETE_EMPLOYEE_ENDPOINT_URL, HttpMethod.DELETE, entity,
+                String.class);
     }
 }
